38f69d90dffeee4614ecde84d3a478248869798d
ridwan-p <ridwan.pamungks@gmail.com>
2023-05-01 20:07:37 +0700

    set salt rounds in hash

--- src/utils/crypt/Hash.js
+++ src/utils/crypt/Hash.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 class Hash {
-  static saltRounds = 10;
+  #saltRounds = 10;
   
   constructor(text) {
     this.setText(text)
@@ -9,12 +9,22 @@ class Hash {
   
   setText(text) {
     if(text) this.text = text
+    return this
+  }
+
+  getSaltRounds() {
+    return this.#saltRounds
+  }
+
+  setSaltRounds(value) {
+    this.#saltRounds = value
+    return this
   }
 
   hash(text) {
     this.setText(text)
     return new Promise((resolve, reject) => {
-      bcrypt.hash(this.text, Hash.saltRounds, function(error, hash) {
+      bcrypt.hash(this.text, this.getSaltRounds(), function(error, hash) {
         if(error) reject(error)
         resolve(hash)
       })
