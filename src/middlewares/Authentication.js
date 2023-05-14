6ee027adbb20e030e374f9d834cd1cc6d4f658cf
pamungkas <ridwan.pamungks@gmail.com>
2023-05-14 15:38:32 +0700

    change status code

--- src/middlewares/Authentication.js
+++ src/middlewares/Authentication.js
@@ -6,7 +6,7 @@ class Authentication {
   async handle(req, res, next) {
     const token = req.body.token || req.query.token || req.headers['authorization'];
     if (!token) {
-      return res.status(403).send('A token is required for authentication');
+      return res.status(401).send('A token is required for authentication');
     }
 
     try {
